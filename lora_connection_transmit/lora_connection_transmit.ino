224aa538f839e4937254ab3b4c558ca86fc53214
PranauR <ajaypranauer10@gmail.com>
2022-07-13 11:21:07 +0530

    WIP: developing transmit sketch

---
+++ lora_connection_transmit/lora_connection_transmit.ino
@@ -0,0 +1,20 @@
+/*
+Module: Lora_connection_transmit.ino
+
+Function:
+        To get the measurement from on-board catena sensor and
+        to transmit those measurement to another catena board using LoRa.
+
+Copyright notice:
+        This file copyright (C) 2022 by
+        MCCI Corporation
+        3520 Krums Corners Road
+        Ithaca, NY 14850
+        An unpublished work. All rights reserved.
+        This file is proprietary information, and may not be disclosed or
+        copied without the prior permission of MCCI Corporation.
+
+Author:
+        Pranau R, MCCI Corporation   July 2022
+*/
+
